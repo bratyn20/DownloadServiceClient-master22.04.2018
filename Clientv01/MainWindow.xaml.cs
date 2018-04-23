c468370077f5240a2af43b985e7b542eedd2ce15
bratyn20 <bratyn20@gmail.com>
2018-04-23 10:34:30 +0700

    Добавлено TRAY меню

--- Clientv01/MainWindow.xaml.cs
+++ Clientv01/MainWindow.xaml.cs
@@ -28,6 +28,129 @@ namespace Clientv01
             InitializeComponent();
         }
 
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e); // базовый функционал приложения в момент запуска
+            createTrayIcon(); // создание нашей иконки
+        }
+
+        private System.Windows.Forms.NotifyIcon TrayIcon = null;
+        private ContextMenu TrayMenu = null;
+
+        private bool createTrayIcon()
+        {
+            bool result = false;
+            if (TrayIcon == null)
+            { 
+                TrayIcon = new System.Windows.Forms.NotifyIcon(); // создаем новую иконку
+                TrayIcon.Icon = Clientv01.Properties.Resources.arrow; // изображение для трея
+                TrayIcon.Text = "Launcher"; // текст подсказки, всплывающей над иконкой
+                TrayMenu = Resources["TrayMenu"] as ContextMenu; 
+
+                
+                TrayIcon.Click += delegate (object sender, EventArgs e) {
+                    if ((e as System.Windows.Forms.MouseEventArgs).Button == System.Windows.Forms.MouseButtons.Left)
+                    {
+                        ShowHideMainWindow(sender, null);
+                    }
+                    else
+                    {
+                        TrayMenu.IsOpen = true; //показываем меню
+                        Activate(); // нужно отдать окну фокус, см. ниже
+                    }
+                };
+                result = true;
+            }
+            else
+            { // все переменные были созданы ранее
+                result = true;
+            }
+            TrayIcon.Visible = true; // делаем иконку видимой в трее
+            return result;
+        }
+
+        private void ShowHideMainWindow(object sender, RoutedEventArgs e)
+        {
+            TrayMenu.IsOpen = false; // спрячем менюшку, если она вдруг видима
+            if (IsVisible)
+            {
+             
+                Hide();
+
+                (TrayMenu.Items[0] as MenuItem).Header = "Show";
+            }
+            else
+            { 
+                Show();
+                
+                (TrayMenu.Items[0] as MenuItem).Header = "Hide";
+                WindowState = CurrentWindowState;
+                Activate(); 
+            }
+        }
+
+        private WindowState fCurrentWindowState = WindowState.Normal;
+        public WindowState CurrentWindowState
+        {
+            get { return fCurrentWindowState; }
+            set { fCurrentWindowState = value; }
+        }
+
+
+        
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e); // системная обработка
+            if (this.WindowState == System.Windows.WindowState.Minimized)
+            {
+               
+                Hide();
+              
+                (TrayMenu.Items[0] as MenuItem).Header = "Show";
+            }
+            else
+            {
+                
+                CurrentWindowState = WindowState;
+            }
+        }
+
+
+        private bool fCanClose = false;
+        public bool CanClose
+        { 
+            get { return fCanClose; }
+            set { fCanClose = value; }
+        }
+
+        
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e); 
+            if (!CanClose)
+            {   
+                e.Cancel = true;
+              
+                CurrentWindowState = this.WindowState;
+               
+                (TrayMenu.Items[0] as MenuItem).Header = "Show";
+                
+                Hide();
+            }
+            else
+            { 
+                TrayIcon.Visible = false;
+            }
+        }
+
+
+        private void MenuExitClick(object sender, RoutedEventArgs e)
+        {
+            CanClose = true;
+            Close();
+        }
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
